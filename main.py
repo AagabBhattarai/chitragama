23384cba0768a90981536da919ca4a47b09d3a85
AagabBhattarai <aagabbhattarai124@gmail.com>
2024-01-25 17:21:32 +0545

    not-working n view

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from two_view import TwoView
-
+import glob
 def main():
-    filepaths = ["fountain/0003.png","fountain/0004.png"]
-    sfm = TwoView(filepaths)
-    matches = sfm.find_good_correspondences()
-    sfm.find_inlier_points(matches)
-    sfm.find_extrinsics_of_camera()
-    sfm.find_3D_of_iniliers()
+    test_directory = "fountain"
+    # filepaths = ["fountain/0000.png","fountain/0001.png"]
+    filepaths = glob.glob(f"{test_directory}/*.png")
+    # filepaths = ['fountain\\0005.png', 'fountain\\0004.png', 'fountain\\0006.png', 'fountain\\0007.png', 'fountain\\0003.png', 'fountain\\0002.png', 'fountain\\0001.png', 'fountain\\0000.png']#, 'fountain\\0008.png', 'fountain\\0009.png', 'fountain\\0010.png']
+
+    print(filepaths)
+    sfm = TwoView()
+    for im1, im2 in zip(filepaths, filepaths[1:]):
+        
+        sfm.process_image([im1, im2])
+        matches = sfm.find_good_correspondences()
+        sfm.find_inlier_points(matches)
+        sfm.find_extrinsics_of_camera()
+        sfm.find_3D_of_iniliers_alt()
+        # sfm.display()
+
+    sfm.start = 0
     sfm.display()
-    
+
 if __name__ == "__main__":
     main()
--- two_view.py
+++ two_view.py
@@ -1,11 +1,68 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
-
+from plyfile import PlyData, PlyElement
 class TwoView:
-    def __init__(self, filepaths: list[str]) -> None:
+    # def __init__(self, filepaths: list[str]) -> None:
+    #     self.rgb_img1 = cv.imread(filepaths[0], cv.IMREAD_COLOR)
+    #     assert self.rgb_img1 is not None, "Image not found in specified file path"
+    #     self.rgb_img2 = cv.imread(filepaths[1], cv.IMREAD_COLOR)
+    #     assert self.rgb_img2 is not None, "Image not found in specified file path"
+    #     self.rgb_img1 = cv.cvtColor(self.rgb_img1, cv.COLOR_BGR2RGB)
+    #     # self.gray_img1 = cv.cvtColor(self.rgb_img1, cv.COLOR_BGR2GRAY)
+    #     # self.gray_img2 = cv.cvtColor(self.rgb_img2, cv.COLOR_BGR2GRAY)
+    #     self.gray_img1 = cv.imread(filepaths[0], cv.IMREAD_GRAYSCALE)     
+    #     self.gray_img2 = cv.imread(filepaths[1], cv.IMREAD_GRAYSCALE)     
+    #     self.sift = cv.SIFT_create()
+    #     self.bf_matcher = cv.BFMatcher()
+    #     #change how you take matrix K, read it from a file or something
+    #     intrinsic_camera_matrix = [[689.87, 0, 380.17],[0, 691.04, 251.70],[0, 0, 1]]
+    #     self.intrinsic_camera_matrix = np.float32(intrinsic_camera_matrix)
+    #     self.transfomation_matrix = None
+
+    #     self.kp1 = None
+    #     self.kp2 = None
+    #     self.des1 = None
+    #     self.des2 = None
+    #     self.inliers_left = None
+    #     self.inliers_right = None
+        
+    #     self.pts_3D = None
+    #     self.pts_3D_color = list()
+    def __init__(self) -> None:
+        self.rgb_img1 = None
+        self.rgb_img2 = None
+        self.gray_img1  = None
+        self.gray_img2 = None
+        self.sift = cv.SIFT_create()
+        self.bf_matcher = cv.BFMatcher()
+        #change how you take matrix K, read it from a file or something
+        intrinsic_camera_matrix = [[689.87, 0, 380.17],[0, 691.04, 251.70],[0, 0, 1]]
+        self.intrinsic_camera_matrix = np.float32(intrinsic_camera_matrix)
+        temp = np.eye(4)
+        self.proj1 =self.intrinsic_camera_matrix @ temp[:3,:4]
+        self.transfomation_matrix = None
+    
+        self.kp1 = None
+        self.kp2 = None
+        self.des1 = None
+        self.des2 = None
+        self.inliers_left = None
+        self.inliers_right = None
+        self.left = None
+        self.right = None
+        
+        self.pts_3D = []
+        self.pts_3D_color = []
+        self.stride = []
+        self.start = 0
+        self.stop = 0
+    
+    def process_image(self, filepaths: list[str]):
+        print(type(filepaths))
         self.rgb_img1 = cv.imread(filepaths[0], cv.IMREAD_COLOR)
         assert self.rgb_img1 is not None, "Image not found in specified file path"
+        
         self.rgb_img2 = cv.imread(filepaths[1], cv.IMREAD_COLOR)
         assert self.rgb_img2 is not None, "Image not found in specified file path"
         self.rgb_img1 = cv.cvtColor(self.rgb_img1, cv.COLOR_BGR2RGB)
@@ -13,22 +70,7 @@ class TwoView:
         # self.gray_img2 = cv.cvtColor(self.rgb_img2, cv.COLOR_BGR2GRAY)
         self.gray_img1 = cv.imread(filepaths[0], cv.IMREAD_GRAYSCALE)     
         self.gray_img2 = cv.imread(filepaths[1], cv.IMREAD_GRAYSCALE)     
-        self.sift = cv.SIFT_create()
-        self.bf_matcher = cv.BFMatcher()
-        #change how you take matrix K, read it from a file or something
-        intrinsic_camera_matrix = [[689.87, 0, 380.17],[0, 691.04, 251.70],[0, 0, 1]]
-        self.intrinsic_camera_matrix = np.float32(intrinsic_camera_matrix)
-        self.transfomation_matrix = None
 
-        self.kp1 = None
-        self.kp2 = None
-        self.des1 = None
-        self.des2 = None
-        self.inliers_left = None
-        self.inliers_right = None
-        
-        self.pts_3D = None
-        self.pts_3D_color = list()
     def find_good_correspondences(self) -> list[cv.DMatch]:
         #find features in each of the images
         self.kp1, self.des1 = self.sift.detectAndCompute(self.gray_img1,None)
@@ -55,6 +97,8 @@ class TwoView:
         matching_points_from_right.reshape(matching_points_from_right.size//2,2)
         self.inliers_left = matching_points_from_left[mask.ravel() == 1].reshape(-1,2)
         self.inliers_right = matching_points_from_right[mask.ravel() == 1].reshape(-1,2)
+        self.left = matching_points_from_left.reshape(-1,2)
+        self.right = matching_points_from_right.reshape(-1,2)
         print(len(matching_points_from_left)) 
     
     def find_extrinsics_of_camera(self) -> None:
@@ -62,7 +106,8 @@ class TwoView:
                                          self.inliers_right,
                                          self.intrinsic_camera_matrix,
                                          cv.RANSAC, prob=0.999, threshold=1.0)
-
+        j = [x for x in mask if x ==1]
+        print(len(j))
         _, R, t, _ =  cv.recoverPose(E,  self.inliers_left,
                                             self.inliers_right,
                                             self.intrinsic_camera_matrix,
@@ -80,7 +125,7 @@ class TwoView:
     def to_camera_coordinate(self, K, point: list[float]) -> list[float]:
         normalized = [  (point[0] - K[0,2]) / K[0,0] ,  (point[1] - K[1,2])/K[1,1] ];
         return normalized
-    def find_3D_of_iniliers(self):
+    def find_3D_of_iniliers(self) -> list[list[float]]:
         pts_left_camera_space = list()
         pts_right_camera_space = list()
         for ptl, ptr in zip(self.inliers_left, self.inliers_right):
@@ -94,17 +139,48 @@ class TwoView:
         proj2 = (self.transfomation_matrix)[0:3, 0:4] 
         
         recovered_3D_points_in_homogenous = cv.triangulatePoints(proj1, proj2, pts_left_camera_space, pts_right_camera_space).T
-        self.pts_3D = recovered_3D_points_in_homogenous[:, 0:3]/recovered_3D_points_in_homogenous[:,3:]
-
+        for pts in recovered_3D_points_in_homogenous[:, 0:3]/recovered_3D_points_in_homogenous[:,3:]:
+            self.pts_3D.append(pts)
+        
+    def find_3D_of_iniliers_alt(self) -> None:
+        temp: np.ndarray = np.eye(4)
+        # x = K * [R|t] X; proj1 = K * [R|t] 
+        # proj1 = self.intrinsic_camera_matrix @ temp[:3, :4]
+        proj1 = self.proj1
+        # x' = K*[R|t] X'; X= X';;;TO DEFINE PROPERLY  
+        proj2 = proj1 @ self.transfomation_matrix
+        
+        # recovered_3D_points_in_homogenous = cv.triangulatePoints(proj1, proj2, self.left.T, self.right.T).T
+        recovered_3D_points_in_homogenous = cv.triangulatePoints(proj1, proj2, self.inliers_left.T, self.inliers_right.T).T
+        
+        self.proj1 = proj2
+        self.stop += recovered_3D_points_in_homogenous.shape[0] 
+        for pts in recovered_3D_points_in_homogenous[:, 0:3]/recovered_3D_points_in_homogenous[:,3:]:
+            self.pts_3D.append(pts)
+        
     def display(self):
+        self.pts_3D = np.float32(self.pts_3D).reshape(-1,3)
+        x,y,z =self.pts_3D[:, 0], self.pts_3D[:,1], self.pts_3D[:, 2] 
+        pts = list(zip(x,y,z))
+        vertex = np.array(pts, dtype=[('x', 'f4'), ('y', 'f4'), ('z', 'f4')])
+
+        el = PlyElement.describe(vertex, 'vertex')
+        PlyData([el]).write("test.ply")
+
         fig = plt.figure(figsize = (10,10))
         ax = plt.axes(projection='3d')
         
-        ax.scatter(self.pts_3D[:, 0], self.pts_3D[:,1], self.pts_3D[:, 2], c= self.pts_3D_color)
+        start = self.start
+        stop = self.stop
+        # ax.scatter(self.pts_3D[self.start:self.stop, 0], self.pts_3D[self.start:self.stop,1], self.pts_3D[self.start:self.stop, 2],s= 1, c= self.pts_3D_color[self.start:self.stop])
+        ax.scatter(self.pts_3D[self.start:self.stop, 0], self.pts_3D[self.start:self.stop,1], self.pts_3D[self.start:self.stop, 2],s= 1)
+        self.start = self.stop
         ax.set_title('3D Parametric Plot')
         ax.set_xlabel('x', labelpad=20)
         ax.set_ylabel('y', labelpad=20)
         ax.set_zlabel('z', labelpad=20)
         plt.show()
-        plt.plot(self.pts_3D[:, 0], self.pts_3D[:,1], '.')
+            
+        # plt.scatter(self.pts_3D[:, 0], self.pts_3D[:,1], marker='.')
+        self.pts_3D = self.pts_3D.tolist()
         plt.show()
